3e1b6a9520cdc452167661f8ba1668bb6387dc1f
Jatin Sharma <38327417+Jatinzorro@users.noreply.github.com>
2021-08-03 12:13:51 +0530

    Add files via upload

---
+++ part11-Part11_05.ThePlayerAndTheBot/src/main/java/Bot.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Jatin Sharma
+ */
+public class Bot extends Player {
+    //private Player player;
+    public void play(){
+        
+    }
+    public void addMove(String move){
+        
+    }
+}
---
+++ part11-Part11_11.ValidatingParameters/src/main/java/validating/Person.java
@@ -0,0 +1,26 @@
+package validating;
+
+public class Person {
+
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        if(name==null||name.equals("")||name.length()>40){
+            throw new IllegalArgumentException("invalid name");
+        }
+        if(age<0 || age>120){
+            throw new IllegalArgumentException("invalid Age");
+        }
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
